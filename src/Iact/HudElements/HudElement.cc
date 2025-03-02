cce8917c396ed3c48337822bf2563456a679a76b
Farley Runkel <farleyrunkel@gmail.com>
2025-03-02 19:12:35 +0800

    patch

---
+++ src/Iact/HudElements/HudElement.cc
@@ -0,0 +1,19 @@
+// Copyright [2024] SunCAD
+
+#include "Iact/HudElements/HudElement.h"
+
+#include "Iact/Workspace/WorkspaceController.h"
+
+WorkspaceController* IHudElement::workspaceController() const
+{
+	return m_workspaceController;
+}
+
+void IHudElement::setWorkspaceController(WorkspaceController* controller)
+{
+	if(m_workspaceController != nullptr && m_workspaceController != controller)
+	{
+		throw std::logic_error("workspaceController cannot be changed");
+	}
+	m_workspaceController = controller;
+}
---
+++ src/Iact/HudElements/HudElement.h
@@ -0,0 +1,31 @@
+// Copyright [2024] SunCAD
+
+#ifndef IACT_HUD_ELEMENTS_HUDELEMENT_H_
+#define IACT_HUD_ELEMENTS_HUDELEMENT_H_
+
+#include <QWidget>
+#include <QSize>
+
+class WorkspaceController;
+
+class IHudElement : public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit IHudElement(QWidget* parent = nullptr)
+		: QWidget(parent)
+		, m_workspaceController(nullptr)
+	{}
+
+public:
+	WorkspaceController* workspaceController() const;
+	void setWorkspaceController(WorkspaceController* controller);
+
+	virtual void initialize() {};
+
+private:
+	WorkspaceController* m_workspaceController;
+};
+
+#endif  // IACT_HUD_ELEMENTS_HUDELEMENT_H_
