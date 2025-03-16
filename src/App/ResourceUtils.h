7e0dd7d5f5f003aa331cf4e2bb249937ad2423ab
Farley Runkel <farleyrunkel@gmail.com>
2025-03-16 13:19:10 +0800

    patch

--- src/App/ResourceUtils.h
+++ src/App/ResourceUtils.h
@@ -1,27 +1,44 @@
-// Resource.h
+// ResourceUtils.h
 #ifndef RESOURCEUTILS_H_
 #define RESOURCEUTILS_H_
 
+// Qt includes
 #include <QString>
 #include <QIcon>
 #include <QFile>
-#include <QDebug>
+#include <QImage>
 
-
-class Resource 
+class Resource
 {
 public:
-    static constexpr const char* ICON_PATH_PREFIX = "://icons/";
+    static constexpr const char* ICON_PATH_PREFIX = "://Icons/";
+    static constexpr const char* MARKER_PATH_PREFIX = "://Marker/";
 
-    static QString IconPath(const QString& iconName) 
+    static QString IconPath(const QString& iconName)
     {
         return QString("%1%2.svg").arg(ICON_PATH_PREFIX, iconName);
     }
 
-    static QIcon Icon(const QString& iconName) 
+    static QString MarkerPath(const QString& iconName)
+    {
+        return QString("%1%2.svg").arg(MARKER_PATH_PREFIX, iconName);
+    }
+
+    static QIcon Icon(const QString& iconName)
     {
         return QIcon(IconPath(iconName));
     }
+
+    static QString GetResourceUri(const QString& filename)
+    {
+        return AssemblyName().append(filename);
+    }
+
+    static QImage ReadBitmapFromResource(const QString& filename)
+    {
+        return QImage(GetResourceUri(filename));
+    }
+
 
 public:
     static bool IsResourcePathValid(const QString& resourcePath)
@@ -29,6 +46,12 @@ public:
         QFile file(resourcePath);
         return file.exists();
     }
+
+private:
+    static QString AssemblyName()
+    {
+        return ":/";
+    };
 };
 
 #endif // RESOURCEUTILS_H_
